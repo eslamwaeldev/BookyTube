2805cd4864a67476acd3cb047db33e892336c3da
Eslam Wael <eslam.wael@cultiv.llc>
2023-06-11 14:46:34 +0300

    fix: fix deleting the first entry deletes 2 entries

--- contentScript.js
+++ contentScript.js
@@ -1,85 +1,98 @@
 import { v4 as uuidv4 } from "uuid";
-(() => {
-  let youtubeLeftControls, youtubePlayer;
-  let currentVideo = new URLSearchParams(window.location.href.split("?")[1]).get("v");
-  let currentVideoBookmarks = [];
 
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, value, videoId, id } = obj;
-    if (type === "NEW") {
-      currentVideo = videoId;
-      newVideoLoaded();
-    } else if (type === "PLAY") {
-      youtubePlayer.currentTime = value;
-    } else if (type === "DELETE") {
-      currentVideoBookmarks = currentVideoBookmarks.filter((bookmark) => bookmark.id !== value);
-      chrome.storage.sync.remove([currentVideo]);
-      if (currentVideoBookmarks.length > 0) {
-        chrome.storage.sync.set({
-          [currentVideo]: JSON.stringify(currentVideoBookmarks),
-        });
-        chrome.storage.sync.get([currentVideo], (result) => {});
-      } else {
-        newVideoLoaded();
+let youtubeLeftControls, youtubePlayer;
+let currentVideo = new URLSearchParams(window.location.href.split("?")[1]).get("v");
+let currentVideoBookmarks = [];
+
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const { type, value, videoId, id } = obj;
+  if (type === "NEW") {
+    currentVideo = videoId;
+    newVideoLoaded();
+  } else if (type === "PLAY") {
+    youtubePlayer.currentTime = value;
+  } else if (type === "DELETE") {
+    currentVideoBookmarks.forEach((entry) => {
+      if (entry.id === value) {
+        console.log(true);
       }
-      response(currentVideoBookmarks);
-    }
-  });
-
-  const fetchBookmarks = () => {
-    return new Promise((resolve) => {
-      chrome.storage.sync.get([currentVideo], (result) => {
-        resolve(result[currentVideo] ? JSON.parse(result[currentVideo]) : []);
+    });
+    console.log(currentVideoBookmarks.length);
+    currentVideoBookmarks = currentVideoBookmarks.filter((bookmark) => bookmark.id !== value);
+    console.log(currentVideoBookmarks.length);
+    chrome.storage.sync.remove([currentVideo]);
+    if (currentVideoBookmarks.length > 0) {
+      chrome.storage.sync.set({
+        [currentVideo]: JSON.stringify(currentVideoBookmarks),
       });
-    });
-  };
-
-  const newVideoLoaded = async () => {
-    const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-    currentVideoBookmarks = await fetchBookmarks();
-
-    if (!bookmarkBtnExists) {
-      const bookmarkBtn = document.createElement("img");
-
-      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-      bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-      bookmarkBtn.title = "Click to bookmark current timestamp";
-      bookmarkBtn.style.width = "3rem";
-      bookmarkBtn.style.height = "3rem";
-      bookmarkBtn.style.marginBottom = "auto";
-      bookmarkBtn.style.marginTop = "auto";
-      bookmarkBtn.style.marginLeft = "auto";
-
-      youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-      youtubePlayer = document.getElementsByClassName("video-stream")[0];
-
-      youtubeLeftControls.append(bookmarkBtn);
-      bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+    } else {
+      newVideoLoaded();
     }
-  };
+    response(currentVideoBookmarks);
+  }
+});
 
-  const addNewBookmarkEventHandler = async () => {
-    const currentTime = youtubePlayer.currentTime;
-    const bookmarkID = uuidv4();
-    console.log(typeof bookmarkID);
-    const newBookmark = {
-      time: currentTime,
-      desc: "Bookmark at " + getTime(currentTime),
-      id: bookmarkID,
-    };
-
-    currentVideoBookmarks = await fetchBookmarks();
-
-    chrome.storage.sync.set({
-      [currentVideo]: JSON.stringify(
-        currentVideoBookmarks.length > 0
-          ? [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
-          : [newBookmark].sort((a, b) => a.time - b.time)
-      ),
+const fetchBookmarks = () => {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get([currentVideo], (result) => {
+      if (result) {
+        resolve(result[currentVideo] ? JSON.parse(result[currentVideo]) : []);
+      }
     });
+  });
+};
+
+const newVideoLoaded = async () => {
+  const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+  currentVideoBookmarks = await fetchBookmarks();
+
+  if (!bookmarkBtnExists) {
+    const bookmarkBtn = document.createElement("img");
+
+    bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+    bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+    bookmarkBtn.title = "Click to bookmark current timestamp";
+    bookmarkBtn.style.width = "3rem";
+    bookmarkBtn.style.height = "3rem";
+    bookmarkBtn.style.marginBottom = "auto";
+    bookmarkBtn.style.marginTop = "auto";
+    bookmarkBtn.style.marginLeft = "auto";
+
+    youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+    youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+    youtubeLeftControls.append(bookmarkBtn);
+    bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+  }
+};
+
+const addNewBookmarkEventHandler = async () => {
+  const currentTime = youtubePlayer.currentTime;
+  const bookmarkID = uuidv4();
+  console.log(typeof bookmarkID);
+  const newBookmark = {
+    time: currentTime,
+    desc: "Bookmark at " + getTime(currentTime),
+    id: bookmarkID,
   };
-  newVideoLoaded();
-})();
+  currentVideoBookmarks = await fetchBookmarks();
+  chrome.storage.sync.set({
+    [currentVideo]: JSON.stringify(
+      currentVideoBookmarks.length > 0
+        ? [...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time)
+        : [newBookmark]
+    ),
+  });
+  currentVideoBookmarks = await fetchBookmarks();
+};
+
+window.addEventListener("load", () => {
+  if (currentVideo) {
+    newVideoLoaded();
+  }
+});
+
+newVideoLoaded();
 
 const getTime = (t) => {
   var date = new Date(0);
