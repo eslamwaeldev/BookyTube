140814c1fab777d241c557e2e5e93eb3aa1e921e
Eslam Wael <eslam.wael104@gmail.com>
2025-05-04 23:44:26 +0300

    fix: defined tabs and query as const as they are never changed

--- src/utils/tabsUtils.ts
+++ src/utils/tabsUtils.ts
@@ -1,7 +1,7 @@
 export const getCurrentTab = async () => {
-  let queryOptions = { active: true, lastFocusedWindow: true };
+  const queryOptions = { active: true, lastFocusedWindow: true };
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let tabs = await chrome.tabs.query(queryOptions);
+  const tabs = await chrome.tabs.query(queryOptions);
   console.log("🚀 ~ getCurrentTab ~ tabs:", tabs);
   return tabs[0];
 };
