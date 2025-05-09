1d2b1667e5fd72e761ef84dd60a9981296efadf6
Eslam Wael <eslam.wael104@gmail.com>
2025-05-09 18:14:33 +0300

    feat: add loading logic and move bookmark fetching

--- src/pages/YoutubeVideoPage.tsx
+++ src/pages/YoutubeVideoPage.tsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
-import { Bookmark } from "../schema";
 import VideoBookmarks from "../components/VideoBookmarks";
+import useVideoBookmarks from "../hooks/useVideoBookmarks";
 
 export interface Props {
   currentVideoId: string;
 }
 
 const YoutubeVideoPage = ({ currentVideoId }: Props) => {
-  const [videoBookmarks, setVideoBookmarks] = useState<Bookmark[]>([]);
-
-  useEffect(() => {
-    chrome.storage.sync.get([currentVideoId], (result) => {
-      setVideoBookmarks(result[currentVideoId] ? JSON.parse(result[currentVideoId]) : []);
-    });
-  }, [currentVideoId]);
+  const { isLoading, videoBookmarks } = useVideoBookmarks(currentVideoId);
 
   return videoBookmarks.length > 0 ? (
-    <VideoBookmarks bookmarks={videoBookmarks} />
+    isLoading ? (
+      <div className="flex flex-col justify-center items-center text-center h-full w-full p-4">
+        <h2 className="text-xl font-bold text-black drop-shadow-header">Loading video Bookmarks</h2>
+      </div>
+    ) : (
+      <VideoBookmarks bookmarks={videoBookmarks} />
+    )
   ) : (
     <div className="flex flex-col justify-center items-center text-center h-full w-full p-4">
       <h2 className="text-xl font-bold text-black drop-shadow-header">
